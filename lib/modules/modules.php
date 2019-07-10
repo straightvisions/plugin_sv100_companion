6fd3b184a1cc570c32691bbf6c19046fee544b37
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-07-10 17:02:14 +0200

    = 1.4.02 = - add support links - add footer settings - add relative time feature

--- lib/modules/modules.php
+++ lib/modules/modules.php
@@ -8,7 +8,14 @@
 		public function init() {
 			$this->sv_cleanup->init();
 			$this->sv_human_time->init();
-			$this->sv_footer_credits->init();
+			
 			$this->freemius->init();
+			
+			add_action('init', array($this, 'wp_init'));
+		}
+		public function wp_init(){
+			if(isset($GLOBALS['sv100'])){
+				$this->sv_footer_credits->init();
+			}
 		}
 	}
