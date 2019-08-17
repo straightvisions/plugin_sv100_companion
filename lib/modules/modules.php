4089d738417693b5028ad3be265d0f79d2d4dd1d
Matthias Bathke <info@straightvisions.com>
2019-08-17 10:24:53 +0200

    MIGRATE Theme Settings Page

--- lib/modules/modules.php
+++ lib/modules/modules.php
@@ -19,5 +19,20 @@
 			if(isset($GLOBALS['sv100'])){
 				$this->sv_footer_credits->init();
 			}
+
+			if(did_action('sv100_init')){
+				add_action( 'admin_menu', array( $this , 'add_theme_page' ), 100 );
+			}
+		}
+		public function add_theme_page(){
+			\add_theme_page(
+				$this->get_instance('sv100')->get_section_title(),		// page title
+				$this->get_instance('sv100')->get_section_title(),		// menu title
+				'edit_theme_options',		// capability
+				$this->get_instance('sv100')->get_prefix(),			// menu slug
+				function(){	// callable function
+					$this->get_instance('sv100')->load_page();
+				}
+			);
 		}
 	}
