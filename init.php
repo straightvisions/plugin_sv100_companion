7e5db59821a17f62f19a33ce0adfa0800d90f88c
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-03-20 11:38:48 +0100

    UPDATE CORE

--- init.php
+++ init.php
@@ -7,7 +7,7 @@
 	
 	class init extends \sv_core\core_plugin {
 		const version				= 1413;
-		const version_core_match	= 4023;
+		const version_core_match	= 4025;
 		
 		public function load(){
 			if(!$this->setup( __NAMESPACE__, __FILE__ )){
